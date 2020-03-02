d33ff9f69e8499fcb9f0b42a16c19d8e9d3af959
Alex Turner <alexwilliamturner@gmail.com>
2020-03-02 16:02:24 +0000

    Changes to project structure. Added social links and a few projects

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.scss"
 
 const Layout = ({ children }) => (
@@ -25,24 +19,10 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 0px 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            <div className="center">
-              © Alex Turner {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </div>
-
-          </footer>
+        <div className="site">
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div className="site-content">{children}</div>
+          <Footer />
         </div>
       </>
     )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,90 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React from 'react'
+import styled from "styled-components"
+import { SocialIcon } from 'react-social-icons'
 import Layout from "../components/layout"
-import Image from "../components/image"
+//import Image from "../components/image"
 import SEO from "../components/seo"
+
+const Projects = styled.div `
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const ProjectItemWrapper = styled.div `
+  align-items: center;
+  display: flex;
+  margin: 0 auto 30px auto;
+  flex-direction: column;
+`
+
+const Title = styled.h4 `
+  margin-bottom: 5px;
+`
+
+const Description = styled.div `
+  margin-bottom: 5px;
+`
+
+const Link = styled.a `
+  text-decoration: none;
+  text-color: black;
+`
+
+const ProjectItem = props => (
+  <ProjectItemWrapper>
+    <Title>{props.title}</Title>
+    <Description>{props.description}</Description>
+    <Link href={props.link}>View</Link>
+  </ProjectItemWrapper>
+)
+
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `alex turner`]} />
     <div className="center">
-      <h1>Alex Turner</h1>
-      <div className="subtitle">
-        <p>
-          <ul>
-            <li>&#123; Developer,&nbsp;</li>
-            <li>Student,&nbsp;</li>
-            <li>Geek &#125;</li>
-          </ul>
+      <div className="header">
+        <h1>Alex Turner</h1>
+        <div className="subtitle">
+          <p className="subtitle">
+            <ul>
+              <li>&#123; Developer,&nbsp;</li>
+              <li>Student,&nbsp;</li>
+              <li>Geek &#125;</li>
+            </ul>
+          </p>
+          <p className="social_links">
+            <ul>
+              <li><SocialIcon url="https://linkedin.com/in/alexwturner"/></li>
+              <li><SocialIcon url="https://github.com/awt95"/></li>
+              <li><SocialIcon url="mailto:alexwilliamturner@gmail.com"/></li>
+            </ul>
+
+          </p>
+        </div>
+      </div>
+      <div className="body">
+        <p className="projects">
+          <h3>Projects</h3>
+          <Projects>
+            <ProjectItem
+              title="Wikibot"
+              description="Chatbot application to answer user queries based on Wikipedia articles. Built using Java, Spring Boot, and DBPedia as datasource."
+              link="https://github.com/awt95/wikibot"
+            />
+            <ProjectItem
+              title="WeQuestit"
+              description="Student startup project. Web application for students to outsource their daily tasks and chores."
+              link="https://github.com/awt95/wequestit-dev"
+            />
+          </Projects>
         </p>
       </div>
+
     </div>
   </Layout>
 )
