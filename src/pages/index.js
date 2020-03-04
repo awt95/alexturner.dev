457adb4395861d69173269c4b79a07584064d2cb
Alex Turner <alexwilliamturner@gmail.com>
2020-03-04 10:22:20 +0000

    Reconfigure as bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Row, Col, Container, ListGroup } from "react-bootstrap"
+import { Row, Col, Container, Card, CardGroup, CardColumns, Button } from "react-bootstrap"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,12 +7,87 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout pageInfo={{ pageName: "index" }}>
     <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
-    <Container className="text-center">
+    <Container className="index-container">
       <Row>
         <Col>
-          <p>
-            My site
-          </p>
+          <h3>Projects</h3>
+          <CardColumns>
+            <Card>
+              <Card.Title><h4>Wikibot</h4></Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">September 2019 - Now</Card.Subtitle>
+              <Card.Body>
+                A web-based chatbot which answers user questions about Wikipedia articles.
+                Implemented using Java and Spring Boot, with DBPedia as its datasource. <br/>
+                Currently being developed for my final year dissertation.
+              </Card.Body>
+              <Card.Footer>
+                <Row>
+                  <Col></Col>
+                  <Col>Status: In Development</Col>
+                  <Col className="text-right text-muted">
+                    <Button className="btn-sm btn-secondary">View</Button>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Title><h4>WeQuestit</h4></Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">June 2018 - June 2019</Card.Subtitle>
+              <Card.Body>
+                A student startup web application project. Allows students to outsource their
+                daily chores and tasks to other student looks to make money. Developed with Ruby on Rails.
+              </Card.Body>
+              <Card.Footer>
+                <Row>
+                  <Col></Col>
+                  <Col>Status: Demo Phase, Inactive</Col>
+                  <Col className="text-right text-muted">
+                    <Button className="btn-sm btn-secondary">View</Button>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Title><h4>ETW Quote System</h4></Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Jan 2015 - Now</Card.Subtitle>
+              <Card.Body>
+                An application to manage a company's quotations, orders, and customers.
+                The application performs quotation calculations, manages current and past orders,
+                and provides the entire business logic for the client. I was brought on as a freelance
+                developer to migrate database and logic from an outdated FoxPro solution.<br/>
+                Technologies used: C# .NET, SQL Server.
+              </Card.Body>
+              <Card.Footer>
+                <Row>
+                  <Col></Col>
+                  <Col>Status: Production</Col>
+                  <Col className="text-right text-muted">
+                    <Button className="btn-sm btn-secondary">View</Button>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+            <Card>
+              <Card.Title><h4>Resource Booker</h4></Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">September 2014 - Now</Card.Subtitle>
+              <Card.Body>
+                A web application to manage user bookings of resources in a school,
+                such as room bookings and IT resources. Client wanted to unify multiple existing systems
+                and allow for more control over privileges. Currently deployed in two local schools. <br/>
+                Technologies: PHP, MySQL.
+              </Card.Body>
+              <Card.Footer>
+                <Row>
+                  <Col></Col>
+                  <Col>Status: Deployed</Col>
+                  <Col className="text-right text-muted">
+                    <Button className="btn-sm btn-secondary">View</Button>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </CardColumns>
+
         </Col>
       </Row>
     </Container>
